95ac75746f8f38ee08d6fb9a9dc7346369f89a2a
Zack Gainsforth <zsg@gainsforth.com>
2023-06-13 16:58:32 -0700

    Bundlizer overview

--- Bundlizer_Overview.py
+++ Bundlizer_Overview.py
@@ -1,2 +1,17 @@
 import streamlit as st
 
+st.title('Bundlizer')
+st.markdown('### Produce bundle files from experimental data for submission to SAMIS.')
+st.markdown('Questions/Problems/Suggestions: contact Zack Gainsforth zackg@berkeley.edu')
+
+infoStr = 'This tool allows you to upload information from an experiment and produce a bundle file for submission to SAMIS.'+\
+'Bundlizer will automatically generate standard data formats and yaml files for your experimental data, '+\
+'assuming there is a valid plugin for your instrument and bundle delivery document (BDD).  '+\
+'Once you have produced a bundle from your raw data, it can be uploaded directly to SAMIS.  '+\
+'Alternately, you can download the raw data plus bundle files for further editing and analysis.  '+\
+'The directory containing raw data plus bundle files can be re-uploaded and used to produce a final bundle file.  '+\
+'(todo youtube video showing how to use)  '+\
+'The following flow chart helps explain the flow:'
+st.write(infoStr)
+
+st.image('BundlizerFlowChart.png')
